5e19efa216eed3a1488cf361346b08589f3488b4
judemartin <judemartin.mj@gmail.com>
2025-04-18 19:01:43 -0500

    agent and llm

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import 'dotenv/config'
-import { runLLM } from './src/llm'
-import { addMessages, getMessages } from './src/memory'
+// import { runLLM } from './src/llm'
+// import { addMessages, getMessages } from './src/memory'
+import { z } from 'zod'
+import { runAgent } from './src/agent.ts'
+
 const userMessage = process.argv[2]
 
 if (!userMessage) {
@@ -8,13 +11,28 @@ if (!userMessage) {
   process.exit(1)
 }
 
-await addMessages([{ role: 'user', content: userMessage }])
-const messages = await getMessages()
 
-const response = await runLLM({
-  messages,
-})
+const weatherTool = {
+  name: 'get_stuff',
+  description: `use this to get this weather`,
+  parameters: z.object({
+    reasoning: z.string().describe('why did you pick this tool?')
+  })
 
-await addMessages([{ role: 'assistant', content: response }])
+}
+
+
+const response = await runAgent({ userMessage, tools: [weatherTool] })
 
 console.log(response)
+//
+// await addMessages([{ role: 'user', content: userMessage }])
+// const messages = await getMessages()
+//
+// const response = await runLLM({
+//   messages,
+// })
+//
+// await addMessages([{ role: 'assistant', content: response }])
+//
+// console.log(response)
