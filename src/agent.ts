eda4ecbb0c0927c5e416bae58b969d0d25fb5aff
judemartin <judemartin.mj@gmail.com>
2025-04-23 23:13:20 -0500

    Function calling implementation

--- src/agent.ts
+++ src/agent.ts
@@ -1,12 +1,13 @@
 import type { AIMessage } from '../types'
-import { addMessages, getMessages } from './memory'
+import { addMessages, getMessages, saveToolResponse } from './memory'
 import { runLLM } from './llm'
 import { showLoader, logMessage } from './ui'
+import { runTool } from './toolRunner.ts'
 
 export const runAgent = async ({
-  userMessage,
-  tools,
-}: {
+                                 userMessage,
+                                 tools
+                               }: {
   userMessage: string
   tools: any[]
 }) => {
@@ -17,11 +18,21 @@ export const runAgent = async ({
 
   const response = await runLLM({ messages: history, tools })
 
-  if (response.tool_calls) {
-    console.log(response.tool_calls)
-  }
 
   await addMessages([response])
+  logMessage(response)
+  if (response.tool_calls) {
+    console.log(response.tool_calls)
+
+    const toolCall = response.tool_calls[0]
+    loader.update(`executing: ${toolCall.function.name}`)
+
+    const toolResponse = await runTool(toolCall, userMessage)
+    await saveToolResponse(toolCall.id, toolResponse)
+
+    loader.update(`executed: ${toolCall.function.name}`)
+  }
+
 
   // logMessage(response)
   loader.stop()
--- src/memory.ts
+++ src/memory.ts
@@ -15,7 +15,7 @@ export const addMetadata = (message: AIMessage) => {
   return {
     ...message,
     id: uuidv4(),
-    createdAt: new Date().toISOString(),
+    createdAt: new Date().toISOString()
   }
 }
 
@@ -25,7 +25,7 @@ export const removeMetadata = (message: MessageWithMetadata) => {
 }
 
 const defaultData: Data = {
-  messages: [],
+  messages: []
 }
 
 export const getDb = async () => {
@@ -42,4 +42,15 @@ export const addMessages = async (messages: AIMessage[]) => {
 export const getMessages = async () => {
   const db = await getDb()
   return db.data.messages.map(removeMetadata)
+}
+
+export const saveToolResponse = async (
+  toolCallId: string,
+  toolResponse: string
+) => {
+  return await addMessages([
+    {
+      role: 'tool', content: toolResponse, tool_call_id: toolCallId
+    }
+  ])
 }
