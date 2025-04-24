5904e19382153e6eede76d85ed92ff36f2e08b34
judemartin <judemartin.mj@gmail.com>
2025-04-23 23:34:49 -0500

    Agents implementation for multiple tasks

--- src/agent.ts
+++ src/agent.ts
@@ -5,31 +5,38 @@ import { showLoader, logMessage } from './ui'
 import { runTool } from './toolRunner'
 
 export const runAgent = async ({
-  userMessage,
-  tools,
-}: {
+                                 userMessage,
+                                 tools
+                               }: {
   userMessage: string
   tools: any[]
 }) => {
   await addMessages([{ role: 'user', content: userMessage }])
 
   const loader = showLoader('🤔')
-  const history = await getMessages()
 
-  const response = await runLLM({ messages: history, tools })
-  await addMessages([response])
+  while (true) {
 
-  if (response.tool_calls) {
-    const toolCall = response.tool_calls[0]
+    const history = await getMessages()
+    const response = await runLLM({ messages: history, tools })
 
-    loader.update(`executing: ${toolCall.function.name}`)
+    await addMessages([response])
 
-    const toolResponse = await runTool(toolCall, userMessage)
-    await saveToolResponse(toolCall.id, toolResponse)
-    loader.update(`done: ${toolCall.function.name}`)
+
+    if (response.content) {
+      loader.stop()
+      logMessage(response)
+      return getMessages()
+    }
+
+    if (response.tool_calls) {
+      const toolCall = response.tool_calls[0]
+      logMessage(response)
+      loader.update(`executing: ${toolCall.function.name}`)
+
+      const toolResponse = await runTool(toolCall, userMessage)
+      await saveToolResponse(toolCall.id, toolResponse)
+      loader.update(`done: ${toolCall.function.name}`)
+    }
   }
-
-  logMessage(response)
-  loader.stop()
-  return getMessages()
 }
